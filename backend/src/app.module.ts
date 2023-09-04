5098a3721616fc4d283a712f6071245e070c3ef3
WAQAS HUSSAIN <waqashussain9075@gmail.com>
2023-09-05 02:08:30 +0500

    mongodb setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './environments/environment';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(environment.mongoBaseUrl),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
