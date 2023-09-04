f10e211fefee2b9072c0e2f426046278c0692324
WAQAS HUSSAIN <waqashussain9075@gmail.com>
2023-09-05 02:13:00 +0500

    user module controller and service added

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common"
+import { MongooseModule } from "@nestjs/mongoose"
+import { UsersController } from "./users.controller"
+import { UserSchema } from "../models/users.model"
+import { UsersService } from "./users.service"
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Model.User, schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
