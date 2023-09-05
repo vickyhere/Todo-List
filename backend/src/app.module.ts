984c7412446f0668e970d4d314ea7b9d96534896
WAQAS HUSSAIN <waqashussain9075@gmail.com>
2023-09-06 02:51:21 +0500

    todo controller service module added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { environment } from './environments/environment';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(environment.mongoBaseUrl),
     UsersModule,
     AuthModule,
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
